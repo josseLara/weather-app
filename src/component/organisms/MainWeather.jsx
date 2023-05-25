2857cdb82e2914a1ef701324e5f3ca982465c748
José Lara <josselara1@gmail.com>
2023-05-25 16:23:34 -0300

    cahnges weather

--- src/component/organisms/MainWeather.jsx
+++ src/component/organisms/MainWeather.jsx
@@ -2,24 +2,41 @@ import { styled } from "styled-components";
 
 function MainWeather() {
      return (
-          <div>
+          <Weather>
+               {/*  */}
                <WeatherLocation>
                     <h1>Montreal</h1>
                     <span>Thursday</span>
                </WeatherLocation>
+               {/*  */}
 
-               
 
-          </div>
+          </Weather>
      );
 }
 
 // ----> styled <-------
+const Weather = styled.div`
+     width: 600px;
+     display: flex;
+     padding: 10px;
+     background-color: #00000044;
+     color: #fff;
+`;
 const WeatherLocation = styled.div`
      display: flex;
      flex-direction: column;
      gap: 10px;
-`
+     
+     h1{
+          font-size: 2.9rem;
+          font-weight: 400;
+     }
+     span{
+          font-size: 1.5rem;
+          font-weight: 200;
+     }
+`;
 
 
 export default MainWeather;
